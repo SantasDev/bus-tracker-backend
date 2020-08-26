5a7e89320659e783bf98a0770d477541b27efc56
Cristian Henao <chenao@playvox.com>
2020-08-25 19:21:45 -0500

    [refactor] modify models strucutre

--- busTrackerBackend/users/models.py
+++ busTrackerBackend/users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
+
 from django.db import models
 
 # Create your models here.
+
+class BTUser(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    contract = models.CharField(max_length=100, blank=True)
+    enterprise = models.CharField(max_length=100, blank=True)
+    role = models.CharField(max_length=100, blank=True)
+
+    Created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
